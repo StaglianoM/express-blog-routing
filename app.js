3914a10bccd61e20c8e7756d6814458c2a4de827
StaglianoM <stagliano.m92@gmail.com>
2024-11-19 21:21:41 +0100

    add router + bonus

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
 const express = require('express');
-const posts = require('./routers/posts'); //collegamento a posts con routers
+const postsRouter = require("./routers/routerPost");
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Home Blog');
-});
-
-// Rotta per ottenere tutti i post
-app.get('/posts', function (req, res) {
-    res.send('Lista');
-});
-
-// show
-app.get('/posts/:id', function (req, res) {
-    res.send('Dettagli ' + req.params.id);
-});
-
-// store
-app.post('/posts', function (req, res) {
-    res.send('Creazione');
-});
-
-// update
-app.put('/posts/:id', function (req, res) {
-    res.send('Modifica integrale ' + req.params.id);
-});
-
-// modify
-app.patch('/posts/:id', function (req, res) {
-    res.send('Modifica parziale ' + req.params.id);
-});
-
-// destroy
-app.delete('/posts/:id', function (req, res) {
-    res.send('Eliminazione ' + req.params.id);
-});
+app.use("/posts", postsRouter);
 
 // Avvio il server
 app.listen(port, () => {
